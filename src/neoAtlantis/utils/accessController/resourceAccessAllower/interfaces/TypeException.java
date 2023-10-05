82d45a2d06a9a44208d3534c5c188c1d08224609
Alberto Sánchez López <aslhiryu@gmail.com>
2023-10-05 12:04:00 -0600

    Actualiza git action

---
+++ src/neoAtlantis/utils/accessController/resourceAccessAllower/interfaces/TypeException.java
@@ -0,0 +1,11 @@
+package neoAtlantis.utils.accessController.resourceAccessAllower.interfaces;
+
+/**
+ *
+ * @author Hiryu (aslhiryu@gmail.com)
+ */
+public enum TypeException {
+    PAGE,
+    RESOURCE,
+    DIRECTORY
+}
